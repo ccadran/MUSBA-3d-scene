4f43bc1319a8a734b66cb99e5f68accfd1b9b7cf
ccadran <clariocadran33@gmail.com>
2024-02-22 17:04:29 +0100

    add custom cursor

--- src/anim.js
+++ src/anim.js
@@ -4,6 +4,7 @@ const loader = document.querySelector(".loader");
 const htmlContainer = document.querySelector(".html-3d-scene");
 const enterExperience = document.querySelector(".enter");
 const cursor = document.querySelector(".cursor");
+const cursor2 = document.querySelector(".cursor2");
 
 enterExperience.addEventListener("click", () => {
   gsap.to(loader, {
@@ -19,6 +20,12 @@ window.addEventListener("mousemove", (e) => {
   gsap.to(cursor, {
     left: e.x,
     top: e.y,
+    duration: 0.3,
+  });
+  gsap.to(cursor2, {
+    left: e.x,
+    top: e.y,
+    duration: 0.4,
   });
 });
 
