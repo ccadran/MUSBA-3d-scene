f000213fb30752904e305f01a2ac51e65839ac60
ccadran <clariocadran33@gmail.com>
2024-02-22 16:47:35 +0100

    add cursor

--- src/anim.js
+++ src/anim.js
@@ -2,13 +2,24 @@ import gsap from "gsap";
 
 const loader = document.querySelector(".loader");
 const htmlContainer = document.querySelector(".html-3d-scene");
+const enterExperience = document.querySelector(".enter");
+const cursor = document.querySelector(".cursor");
 
-setTimeout(() => {
+enterExperience.addEventListener("click", () => {
   gsap.to(loader, {
     y: "100%",
   });
   gsap.set(htmlContainer, {
-    opacity: "100%",
+    display: "flex",
   });
-}, [2000]);
-console.log(loader, htmlContainer);
+});
+console.log(enterExperience, loader, htmlContainer);
+
+window.addEventListener("mousemove", (e) => {
+  gsap.to(cursor, {
+    left: e.x,
+    top: e.y,
+  });
+});
+
+setTimeout(() => {}, [2000]);
